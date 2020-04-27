89914cc5979f9242315ca7a96faed567e7a314e2
Muslim <ikzsl@yandex.ru>
2020-04-27 14:48:57 +0300

    init

---
+++ src/components/Filter.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/index.js';
+
+const filters = [['all', 'All Tasks'], ['active', 'Active Tasks'], ['finished', 'Finished Tasks']];
+
+// BEGIN (write your solution here)
+
+// END
